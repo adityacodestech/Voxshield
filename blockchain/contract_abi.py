3081d7bbf2d8615fbcec7d7da736682aeead3239
mahi-sinha-2006 <mahisinha607@gmail.com>
2026-09-10 03:40:46 +0530

    Add blockchain integration files

---
+++ blockchain/contract_abi.py
@@ -0,0 +1,44 @@
+CONTRACT_ABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "score",
+                "type": "uint256"
+            }
+        ],
+        "name": "calculateRisk",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "user",
+                "type": "address"
+            }
+        ],
+        "name": "getRisk",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "score",
+                "type": "uint256"
+            },
+            {
+                "internalType": "string",
+                "name": "riskLevel",
+                "type": "string"
+            },
+            {
+                "internalType": "uint256",
+                "name": "timestamp",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
